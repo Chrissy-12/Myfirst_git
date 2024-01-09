fbf47ccb1c7a9606732c2f094e5eea3abfb84fe7
Chrissy-12 <christyanna.heath@outlook.com>
2024-01-09 15:27:51 -0500

    mssa calc

---
+++ MSSACalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MSSACalculator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 2-;
+        }
+        int valueAdd = Add(num1, num2);
+        Console.WriteLine($"The sum of {num1} and {num2} is {valueAdd}")
+        public static int Add(int num1, int num2)
+        {
+            public static int Add(int num1. int num2)
+
+            return num1 + num2;
+
+            
+        }
