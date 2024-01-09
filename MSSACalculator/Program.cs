a34679c989c8b8540833e57c0ffdd4432d8d88cd
Chrissy-12 <christyanna.heath@outlook.com>
2024-01-09 15:38:49 -0500

    ch

--- MSSACalculator/Program.cs
+++ MSSACalculator/Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace MSSACalculator
 {
     public class Program
-    {
+}
         public static void Main(string[] args)
         {
             int num1 = 10;
@@ -13,7 +13,4 @@
         {
             public static int Add(int num1. int num2)
 
-            return num1 + num2;
-
-            
-        }
+            return num1 + num2;}
