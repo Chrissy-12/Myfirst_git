f18c08dee57377486a82f6aedd3d9c7c427bd1c9
Chrissy-12 <christyanna.heath@outlook.com>
2024-01-16 15:10:31 -0500

    new

---
+++ [fizzbuzz]/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _fizzbuzz_;
+
+class Program
+{
+    static void Main(string[] args)
+   {   
+        if(x % 3 == 0 && x % 5 == 0);
+        Console.WriteLine("FizzBuzz");
+        else (x % 3 = 0);
+    }
+    // if divide by 3 it will be fizz
+        {
+        Console.WriteLine ("fizz");
+        }
+    // if divides by 5 it will be buzz
+    {
+        else if (x % 5 = 0);
+        Console.WriteLine("Buzz");
+        }
+
+        }
+    
